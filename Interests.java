1793bf0594488f90354638790e8b15d5cfe990f1
Srilakshmi Medarametla <32143967+SriMed@users.noreply.github.com>
2019-04-07 00:06:06 -0400

    Add files via upload

--- Interests.java
+++ Interests.java
@@ -3,6 +3,7 @@ package com.example.capconnect20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Interests extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class Interests extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interests);
+    }
+
+    public void getcampaign(View view) {
+
+        /* get request here */
     }
 }
